73c61a379a927a2d4d975ee6fe45e634b7b6dc24
Sathish9880 <emailAddress=Sathish9880@gmail.com>
2024-02-16 14:51:38 +0530

    add api

---
+++ src/test/java/com/sathish/foodapp/OnlineFoodAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sathish.foodapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
